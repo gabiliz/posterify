630165af517082bf6ef51d17d2e2727005a97f80
gabiliz <gabrielalizmoreira@gmail.com>
2022-08-10 14:49:39 -0300

    build: add secret next auth

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   env: {
     SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
     SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+    NETX_PUBLIC_SECRET: process.env.NEXT_PUBLIC_SECRET,
   },
 }
 
